14906fe6f71a2ac0fc9c4282d8e10481efc21933
Ahmad Abu Hazza <37190336+SasukeKite@users.noreply.github.com>
2024-11-02 11:52:54 +0200

    Start On Create Module Schema

---
+++ AppCommon/DTOs/Modules/ModuleDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCommon.DTOs.Modules
+{
+    public class ModuleDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        //public ModuleTypeEnum Type { get; set; }
+        public string Name { get; set; }
+        public string Domain { get; set; }
+        public bool IsActive { get; set; }
+        public int Order { get; set; }
+        public string Details { get; set; }
+        public Guid? ApplicationId { get; set; }
+    }
+}
---
+++ Application/Features/ControlPanel/Modules/Mapper/ModuleMapper.cs
@@ -0,0 +1,16 @@
+﻿using AppCommon.DTOs.Modules;
+using Application.Common.Mapper;
+using Application.Features.ControlPanel.Modules.Commands;
+using Riok.Mapperly.Abstractions;
+using Module.Domain.Schema;
+
+namespace Application.Features.ControlPanel.Modules.Mapping
+{
+    [Mapper]
+    public partial class ModuleMapper : BaseMapper
+    {
+        public partial ModuleDto MapToDto(Module.Domain.Schema.Module source);
+
+        public partial Module.Domain.Schema.Module MapToEntity(CreateModuleCommand source);
+    }
+}
